47a239e3a5f24a12b14a87590a5af424fe91d30a
makhmudovdev <xursandmadiyarov2004@gmail.com>
2024-02-02 00:02:52 +0500

    login, loader and dropdown end

--- index.js
+++ index.js
@@ -97,3 +97,150 @@ function cecButtonp2() {
     }
     caruselImgep.style.transform = `translateX(${-indxp * 1230}px)`;
 }
+
+
+
+
+let loadWrapper = document.querySelector('.load_wrapper');
+
+
+
+window.addEventListener('DOMContentLoaded',()=>{
+    loadWrapper.style.display = 'flex';
+    document.body.style.overflow="hidden";
+})
+
+
+window.addEventListener('load',()=>{
+
+    setTimeout( ()=>{
+
+        loadWrapper.style.display = 'none';
+        document.body.style.overflow="visible";
+
+    } , 2000)
+  
+});
+
+
+
+
+
+
+let modalWrapper = document.querySelector(".modal_wrapper"),
+open=document.querySelector("#login"),
+close=document.querySelector("#close");
+
+
+open.addEventListener("click",()=>{
+    modalWrapper.style.display = "flex";
+    document.body.style.overflow = "hidden";
+})
+
+
+close.addEventListener("click",()=>{
+    modalWrapper.style.display = "none";
+    document.body.style.overflow = "visible";
+})
+
+modalWrapper.addEventListener('click', (e)=>{
+
+
+    if( e.target.classList.contains('modal_header') || e.target.classList.contains('modal_body') || e.target.classList.contains('modal_footer') || e.target.classList.contains('modal_title') || e.target.classList.contains("modal_submit")){
+        modalWrapper.style.display = "flex";
+        console.log(e.target.classList);
+        document.body.style.overflow = "hidden";
+    }else{
+        modalWrapper.style.display = "none";
+        console.log(e.target.classList)
+        document.body.style.overflow = "visible";
+    }
+
+});
+
+
+
+
+///---------------------------------MODAL CONTENT 
+
+let modalWrappers = document.querySelector(".modal_wrappers"),
+kirish=document.querySelector("#kirish"),
+chiqish=document.querySelector("#chiqish");
+
+
+kirish.addEventListener("click",()=>{
+    modalWrappers.style.display = "block";
+    document.body.style.overflow = "hidden";
+})
+
+
+chiqish.addEventListener("click",()=>{
+    modalWrappers.style.display = "none";
+    document.body.style.overflow = "visible";
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let modalWrappers = document.querySelector(".modal_wrappers"),
+// opens=document.querySelector("#logins"),
+// closes=document.querySelector("#closes");
+
+
+// open.addEventListener("click",()=>{
+//     modalWrappers.style.display = "flex";
+//     document.body.style.overflow = "hidden";
+// })
+
+
+// close.addEventListener("click",()=>{
+//     modalWrappers.style.display = "none";
+//     document.body.style.overflow = "visible";
+// })
+
+// modalWrappers.addEventListener('click', (e)=>{
+
+
+//     if( e.target.classList.contains('modal_headers') || e.target.classList.contains('modal_bodys') || e.target.classList.contains('modal_footers') || e.target.classList.contains('modal_titles') || e.target.classList.contains("modal_submits")){
+//         modalWrappers.style.display = "flex";
+//         console.log(e.target.classList);
+//         document.body.style.overflow = "hidden";
+//     }else{
+//         modalWrappers.style.display = "none";
+//         console.log(e.target.classList)
+//         document.body.style.overflow = "visible";
+//     }
+
+// });
